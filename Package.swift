da186133fe9164fcdc67bf224ceda40aa64c27e0
Vincent <thisoneis4business@gmail.com>
2021-12-08 02:48:30 +0700

    Fix: Updated packages to remove failed builds

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/GraphQLSwift/GraphQL.git", from: "2.0.0"),
         .package(url: "https://github.com/GraphQLSwift/Graphiti.git", from: "1.0.0"),
-        .package(url: "https://github.com/d-exclaimation/desolate.git", from: "1.3.0"),
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.53.0")
+        .package(url: "https://github.com/d-exclaimation/desolate.git", from: "1.3.1"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.54.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
