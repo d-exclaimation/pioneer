d28803904f3c520106130bb7fe8bcef7f97158d7
Vincent <thisoneis4business@gmail.com>
2021-12-30 15:08:54 +0700

    Feat: Added test for AsyncPubSub

--- Tests/PioneerTests/DataStructureTests/AsyncPubSubTests.swift
+++ Tests/PioneerTests/DataStructureTests/AsyncPubSubTests.swift
@@ -1,7 +1,88 @@
-//  File.swift
-//  
 //
-//  Created by d-exclaimation on 12/30/21.
+//  AsyncPubSubTests.swift
+//  PioneerTests
+//
+//  Created by d-exclaimation.
 //
 
 import Foundation
+import XCTest
+@testable import Pioneer
+
+final class AsyncPubSubTests: XCTestCase {
+    /// AsyncPubSub getting `AsyncStream` and publishing data
+    /// - Should be able to receive data from all AsyncStream with the same trigger
+    /// - Should be able to filter published data to only the same type
+    /// - Should be able to publish data after the consumers were set up
+    func testPublishingAndConsuming() async {
+        let pubsub = AsyncPubSub()
+        let trigger = "1"
+        let exp0 = XCTestExpectation()
+        let exp1 = XCTestExpectation()
+        
+        let task = Task {
+            let stream = pubsub.asyncStream(Int.self, for: trigger)
+            for await each in stream {
+                if each == 0 {
+                    exp0.fulfill()
+                }
+                return
+            }
+        }
+        
+        let task1 = Task {
+            let stream = pubsub.asyncStream(Int.self, for: trigger)
+            for await each in stream {
+                if each == 0 {
+                    exp1.fulfill()
+                }
+                return
+            }
+        }
+        
+        try? await Task.sleep(nanoseconds: 1_000_000)
+        
+        await pubsub.publish(for: trigger, payload: "invalid")
+        await pubsub.publish(for: trigger, payload: 0)
+        
+        wait(for: [exp0, exp1], timeout: 2)
+        
+        task.cancel()
+        task1.cancel()
+    }
+    
+    /// AsyncPubSub closing all consumer for a specific trigger
+    /// - Should close all consumer with the same trigger
+    /// - Should never receive anything from any consumer
+    func testClosing() async {
+        let pubsub = AsyncPubSub()
+        let trigger = "1"
+        let exp0 = XCTestExpectation()
+        let exp1 = XCTestExpectation()
+        
+        let task = Task {
+            let stream = pubsub.asyncStream(Bool.self, for: trigger)
+            for await _ in stream {
+                return
+            }
+            exp0.fulfill()
+        }
+        
+        let task1 = Task {
+            let stream = pubsub.asyncStream(Bool.self, for: trigger)
+            for await _ in stream {
+                return
+            }
+            exp1.fulfill()
+        }
+        
+        try? await Task.sleep(nanoseconds: 1_000_000)
+        
+        await pubsub.close(for: trigger)
+        
+        wait(for: [exp0, exp1], timeout: 2)
+        
+        task.cancel()
+        task1.cancel()
+    }
+}
