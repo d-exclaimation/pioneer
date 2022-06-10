f3d1febb11d1f17b1961f5ddd5ffae924668c941
Vincent <thisoneis4business@gmail.com>
2022-06-10 20:34:09 +0700

    Fix: Updated vapor to use the latest non-vunerable version

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         .package(url: "https://github.com/GraphQLSwift/GraphQL.git", from: "2.2.0"),
         .package(url: "https://github.com/GraphQLSwift/DataLoader.git", from: "2.0.0"),
         .package(url: "https://github.com/GraphQLSwift/Graphiti.git", from: "1.0.0"),
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.61.0")
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.61.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
