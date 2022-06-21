d09f2a9fb75f122a828b4ce1d3a7f8d7c80f5450
Vincent <thisoneis4business@gmail.com>
2022-06-21 17:28:14 +0700

    Feat: Changed Broadcast to an actor instead

--- Sources/Pioneer/Streaming/AsyncPubSub.swift
+++ Sources/Pioneer/Streaming/AsyncPubSub.swift
@@ -8,11 +8,11 @@
 import Foundation
 
 /// AsyncPubSub is a in memory pubsub structure for managing AsyncStreams in a concurrent safe way utilizing Actors.
-public struct AsyncPubSub: PubSub, Broadcast, Sendable {
-    public typealias Consumer = AsyncStream<Sendable>.Continuation
+public struct AsyncPubSub: PubSub, Sendable {
+    public typealias Producer = Broadcast<Sendable>
     
     /// Dispatcher is a actor for the pubsub to concurrently manage publishers and incoming data
-    public actor Dispatcher: Dispatch {
+    public actor Dispatcher {
         private var emitters: [String: Producer] = [:]
         
         /// Subscribe get the emitters that is assigned to the given key 
@@ -29,76 +29,65 @@ public struct AsyncPubSub: PubSub, Broadcast, Sendable {
         /// Async stream return a new AsyncStream that is connected to the emitter that is assigned to the given key
         /// - Parameter key: The string topic / key used to find the emitter
         /// - Returns: An async stream that is linked to an emitter
-        public func asyncStream(for key: String) async -> AsyncStream<Sendable> {
+        internal func asyncStream(for key: String) async -> AsyncStream<Sendable> {
             let emitter = await subscribe(for: key)
-            let id = UUID().uuidString.lowercased()
-            return AsyncStream<Sendable> { con in
-                con.onTermination = { @Sendable _ in
-                    Task {
-                        await emitter.unsubscribe(id)
-                    }
-                }
-                
-                Task {
-                    await emitter.subscribe(id, with: con)
-                }
-            }
-            
+            let downstream = await emitter.downstream()
+            return downstream.stream
         }
         
         /// Publish sends a data to a emitter that is assigned to the given key
         /// - Parameters:
         ///   - key: The string topic / key used to find the emitter
         ///   - value: The sendable data being sent
-        public func publish(for key: String, _ value: Sendable) async {
+        internal func publish(for key: String, _ value: Sendable) async {
             await emitters[key]?.publish(value)
         }
         
         /// Close shutdowns an emitter that is assigned to the given key
         /// - Parameter key: The string / topic key
-        public func close(for key: String) async {
+        internal func close(for key: String) async {
             await emitters[key]?.close()
             emitters.delete(key)
         }
         
     }
     
-    /// Emitter is an actor handling a single type, single topic, and multiple consumer concurrent data broadcasting
-    public actor Producer {
-        private var consumers: [String: Consumer] = [:]
-        
-        /// Subscribe saved and set up Consumer to receive broadcasted Sendable data
-        /// - Parameters:
-        ///   - key: The key used to identified the consumer
-        ///   - consumer: The AsyncStream Continuation as the consumer
-        internal func subscribe(_ key: String, with consumer: Consumer) {
-            consumers.update(key, with: consumer)
-        }
-
-        /// Unsubscribe removed the Consumer and prevent it from receiving any further broadcasted data
-        /// - Parameter key: The key used to identified the consumer
-        internal func unsubscribe(_ key: String) {
-            consumers.delete(key)
-        }
-        
-        /// Publish broadcast sendable data to all currently saved consumer
-        /// - Parameter value: The sendable data to be published
-        internal func publish(_ value: Sendable) {
-            consumers.values.forEach { consumer in
-                consumer.yield(value)
-            }
-        }
-        
-        /// Close shutdowns the entire emitter and unsubscribe all consumer
-        internal func close() {
-            consumers.values.forEach { consumer in
-                consumer.finish()
-            }
-            consumers.removeAll()
-        }
-    }
-    
     public let dispatcher: Dispatcher = .init()
     
     public init() {}
+    
+    /// Returns a new AsyncStream with the correct type and for a specific trigger
+    ///
+    /// - Parameters:
+    ///   - type: DataType of this AsyncStream
+    ///   - trigger: The topic string used to differentiate what data should this stream be accepting
+    public func asyncStream<DataType: Sendable>(_ type: DataType.Type = DataType.self, for trigger: String) -> AsyncStream<DataType> {
+        AsyncStream<DataType> { con in
+            let task = Task {
+                let pipe = await dispatcher.asyncStream(for: trigger)
+                for await untyped in pipe {
+                    guard let typed = untyped as? DataType else { continue }
+                    con.yield(typed)
+                }
+                con.finish()
+            }
+            con.onTermination = { @Sendable _ in
+                task.cancel()
+            }
+        }
+    }
+    
+    /// Publish a new data into the pubsub for a specific trigger.
+    /// - Parameters:
+    ///   - trigger: The trigger this data will be published to
+    ///   - payload: The data being emitted
+    public func publish(for trigger: String, payload: Sendable) async {
+        await dispatcher.publish(for: trigger, payload)
+    }
+    
+    /// Close a specific trigger and deallocate every consumer of that trigger
+    /// - Parameter trigger: The trigger this call takes effect on
+    public func close(for trigger: String) async {
+        await dispatcher.close(for: trigger)
+    }
 }
--- Sources/Pioneer/Streaming/Broadcast.swift
+++ Sources/Pioneer/Streaming/Broadcast.swift
@@ -1,4 +1,4 @@
-//  ActorPubSub.swift
+//  Broadcast.swift
 //  
 //
 //  Created by d-exclaimation on 20/06/22.
@@ -6,30 +6,87 @@
 
 import Foundation
 
-/// A PubSub that utilize broadcast hub through an Actor
-public protocol Broadcast where Self: PubSub {
-    /// Dispatcher is a actor for the pubsub to concurrently manage publishers and incoming data
-    associatedtype Dispatcher: Dispatch
+/// An actor to broadcast messages to multiple downstream from a single upstream
+public actor Broadcast<MessageType: Sendable> {
+    public typealias Consumer = AsyncStream<MessageType>.Continuation
     
-    /// The dispatcher actor instance
-    var dispatcher: Dispatcher { get }
-}
-
-/// An actor for a pubsub to concurrently manage publishers, incoming data, and dispatch data
-public protocol Dispatch where Self: Actor {
-    /// Async stream return a new AsyncStream that is connected to the emitter that is assigned to the given key
-    /// - Parameter key: The string topic / key used to find the emitter
-    /// - Returns: An async stream that is linked to an emitter
-    func asyncStream(for key: String) async -> AsyncStream<Sendable>
+    private var consumers: [UUID: Consumer] = [:]
     
-    /// Publish sends a data to a emitter that is assigned to the given key
+    /// Creates a new downstream with an id
+    /// - Returns: The async stream and its id
+    public func downstream() async -> Downstream<MessageType> {
+        Downstream<MessageType> { id, con in
+            Task {
+                await self.subscribe(id, with: con)
+            }
+            
+            con.onTermination = { @Sendable _ in
+                Task {
+                    await self.unsubscribe(id)
+                }
+            }
+        }
+    }
+    
+    /// Unsubscribe removed the downstream and prevent it from receiving any further broadcasted data
+    /// - Parameter downstream: The key used to identified the consumer
+    internal func unsubscribe(_ downstream: Downstream<MessageType>) async {
+        consumers.delete(downstream.id)
+    }
+    
+    /// Unsubscribe removed the downstream and prevent it from receiving any further broadcasted data
+    /// - Parameter id: The key used to identified the consumer
+    internal func unsubscribe(_ id: UUID) async {
+        consumers.delete(id)
+    }
+    
+    /// Subscribe saved and set up downstream to receive broadcasted message
     /// - Parameters:
-    ///   - key: The string topic / key used to find the emitter
-    ///   - value: The sendable data being sent
-    func publish(for key: String, _ value: Sendable) async
+    ///   - id: The key used to identified the consumer
+    ///   - consumer: The AsyncStream Continuation as the consumer
+    internal func subscribe(_ id: UUID, with downstream: Consumer) async {
+        consumers.update(id, with: downstream)
+    }
+
     
-    /// Close shutdowns an emitter that is assigned to the given key
-    /// - Parameter key: The string / topic key
-    func close(for key: String) async 
+    /// Publish broadcast sendable data to all currently saved consumer
+    /// - Parameter value: The sendable data to be published
+    public func publish(_ value: MessageType) async {
+        consumers.values.forEach { consumer in
+            consumer.yield(value)
+        }
+    }
+    
+    /// Close shutdowns the entire broadcast and unsubscribe all consumer
+    public func close() async {
+        consumers.values.forEach { consumer in
+            consumer.finish()
+        }
+        consumers.removeAll()
+    }
+    
 }
 
+/// An async stream with an id
+public struct Downstream<Element: Sendable>: AsyncSequence {
+    /// The id of the stream
+    public let id: UUID
+    /// The stream itself
+    public let stream: AsyncStream<Element>
+    
+    public init(
+        _ elementType: Element.Type = Element.self,
+        bufferingPolicy limit: AsyncStream<Element>.Continuation.BufferingPolicy = .unbounded,
+        _ build: @escaping (UUID, AsyncStream<Element>.Continuation) -> Void
+    ) {
+        let id = UUID()
+        self.id = id
+        self.stream = AsyncStream<Element>(elementType, bufferingPolicy: limit) { con in
+            build(id, con)
+        }
+    }
+    
+    public func makeAsyncIterator() -> AsyncStream<Element>.AsyncIterator {
+        stream.makeAsyncIterator()
+    }
+}
