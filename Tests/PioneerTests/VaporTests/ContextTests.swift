ba150546779044f5e3de98a943caec010e8c0d88
Vincent <thisoneis4business@gmail.com>
2022-06-29 18:28:30 +0700

    Feat: ContextTests for ContextBuilder

---
+++ Tests/PioneerTests/VaporTests/ContextTests.swift
@@ -0,0 +1,73 @@
+//
+//  ContextTests.swift
+//  Pioneer
+//
+//  Created by d-exclaimation on 18:06.
+//
+
+import XCTest
+import Graphiti
+import Vapor
+import NIOFoundationCompat
+import XCTVapor
+@testable import Pioneer
+
+final class ContextTests: XCTestCase {
+    private let server: Pioneer<Resolver, Context> = Pioneer(
+        schema: try! Schema<Resolver, Context> {
+            ID.asScalar()
+
+            Query {
+                Field("test", at: Resolver.test)
+            }
+        }, 
+        resolver: .init(),
+        contextBuilder: { req, res in 
+            guard let authorization = req.headers[.authorization].first else {
+                throw Abort(.unauthorized, reason: "Cannot authoriza user")
+            }
+            guard authorization.contains("Bearer "), let token = authorization.split(separator: " ").last?.description else {
+                throw Abort(.unauthorized, reason: "Cannot authoriza user")
+            }
+            return Context(auth: token)
+        },
+        httpStrategy: .both,
+        introspection: true
+    )
+
+    struct Context {
+        var auth: String
+    }
+
+    struct Resolver {
+        func test(ctx: Context, _: NoArguments) async -> ID {
+            .init(ctx.auth)
+        }
+    }
+
+    /// Context Builder:
+    /// - Should get the proper context given the right headers
+    /// - Should rethrow error from the context builder
+    func testContextBuilderThrowing() throws {
+        let app = Application(.testing)
+        defer {
+            app.shutdown()
+        }
+
+        try app.testable().test(
+            .GET, 
+            "/graphql?query=\("query { test }".addingPercentEncoding(withAllowedCharacters: .alphanumerics)!)",
+            headers: .init([("Authorization", "Bearer Hello")])
+        ) { res in 
+            XCTAssertEqual(res.status, .ok)
+            XCTAssert(res.body.string.contains("Hello"))
+        }
+
+        try app.testable().test(
+            .GET, 
+            "/graphql?query=\("query { test }".addingPercentEncoding(withAllowedCharacters: .alphanumerics)!)"
+        ) { res in 
+            XCTAssertEqual(res.status, .unauthorized)
+        }
+    }
+}
