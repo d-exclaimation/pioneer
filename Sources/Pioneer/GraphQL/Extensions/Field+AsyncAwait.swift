0649375b4fc31d8f6489065d3557013839814122
Vincent <thisoneis4business@gmail.com>
2022-07-19 21:04:07 +1200

    Chore: Remove the extensions to prevent abiguity

--- Sources/Pioneer/GraphQL/Extensions/Field+AsyncAwait.swift
+++ Sources/Pioneer/GraphQL/Extensions/Field+AsyncAwait.swift
@@ -9,12 +9,6 @@ import Graphiti
 import protocol NIO.EventLoopGroup
 import GraphQL
 
-/// Async-await non-throwing GraphQL resolver function
-public typealias AsyncAwaitResolve<ObjectType, Context, Arguments, FieldType> = (ObjectType) -> (Context, Arguments) async -> FieldType
-
-/// Async-await throwing GraphQL resolver function
-public typealias AsyncAwaitThrowingResolve<ObjectType, Context, Arguments, FieldType> = (ObjectType) -> (Context, Arguments) async throws -> FieldType
-
 /// Async-await non-throwing  GraphQL resolver function
 public typealias AsyncAwaitResolveWithEventLoop<ObjectType, Context, Arguments, FieldType> = (ObjectType) -> (Context, Arguments, EventLoopGroup) async -> FieldType
 
@@ -22,64 +16,6 @@ public typealias AsyncAwaitResolveWithEventLoop<ObjectType, Context, Arguments, 
 public typealias AsyncAwaitThrowingResolveWithEventLoop<ObjectType, Context, Arguments, FieldType> = (ObjectType) -> (Context, Arguments, EventLoopGroup) async throws -> FieldType
 
 public extension Graphiti.Field where FieldType : Encodable {
-    // -- (context, args) async -> result
-    
-    convenience init(
-        _ name: String,
-        at function: @escaping AsyncAwaitResolve<ObjectType, Context, Arguments, FieldType>,
-        @ArgumentComponentBuilder<Arguments> _ argument: () -> [ArgumentComponent<Arguments>]
-    ) {
-        let resolve: AsyncResolve<ObjectType, Context, Arguments, FieldType> = { type in
-            { context, arguments, eventLoopGroup in
-                eventLoopGroup.task { await function(type)(context, arguments) }
-            }
-        }
-
-        self.init(name, at: resolve, argument)
-    }
-
-    convenience init(
-        _ name: String,
-        at function: @escaping AsyncAwaitResolve<ObjectType, Context, Arguments, FieldType>
-    ) where Arguments == NoArguments {
-        let resolve: AsyncResolve<ObjectType, Context, Arguments, FieldType> = { type in
-            { context, arguments, eventLoopGroup in
-                eventLoopGroup.task { await function(type)(context, arguments) }
-            }
-        }
-
-        self.init(name, at: resolve, {})
-    }
-
-    // -- (context, args) async throws -> result
-
-    convenience init(
-        _ name: String,
-        at function: @escaping AsyncAwaitThrowingResolve<ObjectType, Context, Arguments, FieldType>,
-        @ArgumentComponentBuilder<Arguments> _ argument: () -> [ArgumentComponent<Arguments>]
-    ) {
-        let resolve: AsyncResolve<ObjectType, Context, Arguments, FieldType> = { type in
-            { context, arguments, eventLoopGroup in
-                eventLoopGroup.task { try await function(type)(context, arguments) }
-            }
-        }
-
-        self.init(name, at: resolve, argument)
-    }
-
-    convenience init(
-        _ name: String,
-        at function: @escaping AsyncAwaitThrowingResolve<ObjectType, Context, Arguments, FieldType>
-    ) where Arguments == NoArguments {
-        let resolve: AsyncResolve<ObjectType, Context, Arguments, FieldType> = { type in
-            { context, arguments, eventLoopGroup in
-                eventLoopGroup.task { try await function(type)(context, arguments) }
-            }
-        }
-
-        self.init(name, at: resolve, {})
-    }
-
     // -- (context, args, eventLoop) async -> result
 
     convenience init(
@@ -146,68 +82,6 @@ public extension Graphiti.Field where FieldType : Encodable {
 
 
 public extension Graphiti.Field {
-    // -- (context, args) async -> result
-    
-    convenience init<ResolveType>(
-        _ name: String,
-        at function: @escaping AsyncAwaitResolve<ObjectType, Context, Arguments, ResolveType>,
-        as: FieldType.Type,
-        @ArgumentComponentBuilder<Arguments> _ argument: () -> [ArgumentComponent<Arguments>]
-    ) {
-        let resolve: AsyncResolve<ObjectType, Context, Arguments, ResolveType> = { type in
-            { context, arguments, eventLoopGroup in
-                eventLoopGroup.task { await function(type)(context, arguments) }
-            }
-        }
-
-        self.init(name, at: resolve, as: `as`, argument)
-    }
-
-    convenience init<ResolveType>(
-        _ name: String,
-        at function: @escaping AsyncAwaitResolve<ObjectType, Context, Arguments, ResolveType>,
-        as: FieldType.Type
-    ) where Arguments == NoArguments {
-        let resolve: AsyncResolve<ObjectType, Context, Arguments, ResolveType> = { type in
-            { context, arguments, eventLoopGroup in
-                eventLoopGroup.task { await function(type)(context, arguments) }
-            }
-        }
-        
-        self.init(name, at: resolve, as: `as`, {})
-    }
-
-    // -- (context, args) async throws -> result
-
-    convenience init<ResolveType>(
-        _ name: String,
-        at function: @escaping AsyncAwaitThrowingResolve<ObjectType, Context, Arguments, ResolveType>,
-        as: FieldType.Type,
-        @ArgumentComponentBuilder<Arguments> _ argument: () -> [ArgumentComponent<Arguments>]
-    ) {
-        let resolve: AsyncResolve<ObjectType, Context, Arguments, ResolveType> = { type in
-            { context, arguments, eventLoopGroup in
-                eventLoopGroup.task { try await function(type)(context, arguments) }
-            }
-        }
-
-        self.init(name, at: resolve, as: `as`, argument)
-    }
-
-    convenience init<ResolveType>(
-        _ name: String,
-        at function: @escaping AsyncAwaitThrowingResolve<ObjectType, Context, Arguments, ResolveType>,
-        as: FieldType.Type
-    ) where Arguments == NoArguments {
-        let resolve: AsyncResolve<ObjectType, Context, Arguments, ResolveType> = { type in
-            { context, arguments, eventLoopGroup in
-                eventLoopGroup.task { try await function(type)(context, arguments) }
-            }
-        }
-
-        self.init(name, at: resolve, as: `as`, {})
-    }
-
     // -- (context, args, eventLoop) async -> result
 
     convenience init<ResolveType>(
