92da57322df01a9b3bc1a5623e6129b69ea892f1
Vincent <thisoneis4business@gmail.com>
2022-11-03 13:29:47 +1300

    chore: Added explaination on how csrf is prevented

--- Sources/Pioneer/Http/CSRFProtections.swift
+++ Sources/Pioneer/Http/CSRFProtections.swift
@@ -12,16 +12,19 @@ public extension Pioneer {
     /// - Parameter headers: HTTP Headers by NIO standard
     /// - Returns: True if vulnerable
     func csrfVunerable(given headers: HTTPHeaders) -> Bool {
-        // If CSRF Prevention is disabled, it is deemed not vunelrable
+        // If CSRF Prevention is disabled, it is deemed not vulnerable
         guard case .csrfPrevention = httpStrategy else {
             return false
         }
 
+        // If any of these two header is given, ignore all other headers and deemed non-vulnerable
         let hasPreflight = !headers[HTTPHeaders.Name("Apollo-Require-Preflight")].isEmpty
         let hasOperationName = !headers[HTTPHeaders.Name("X-Apollo-Operation-Name")].isEmpty
         if hasPreflight || hasOperationName {
             return false
         }
+
+        // CSRF and XS-Search attacks take advantage of simple HTTP request which uses these content-types
         let restrictedHeaders = ["text/plain", "application/x-www-form-urlencoded", "multipart/form-data"]
         let contentTypes = headers[.contentType]
         return contentTypes.contains { contentType in
