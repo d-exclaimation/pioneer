33df59c6bb52b6a47b0fe163c4771cc1d5d480a1
Vincent <thisoneis4business@gmail.com>
2022-11-26 21:02:42 +1300

    fix(http): Fixed HTTPGraphQLRequest/Response
    - Made HTTPGraphQLRequest and HTTPGraphQLResponse's initialiser public

--- Sources/Pioneer/Http/HTTPGraphQL.swift
+++ Sources/Pioneer/Http/HTTPGraphQL.swift
@@ -7,8 +7,10 @@
 
 import enum NIOHTTP1.HTTPResponseStatus
 import enum NIOHTTP1.HTTPMethod
+import enum GraphQL.Map
 import struct NIOHTTP1.HTTPHeaders
 import struct GraphQL.GraphQLResult
+import struct GraphQL.GraphQLError
 
 extension Pioneer {
     /// HTTP-based GraphQL Response
@@ -18,6 +20,16 @@ extension Pioneer {
 
         /// HTTP status code for this response
         public var status: HTTPResponseStatus
+
+        public init(result: GraphQLResult, status: HTTPResponseStatus) {
+            self.result = result
+            self.status = status
+        }
+
+        public init(data: Map? = nil, errors: [GraphQLError] = [], status: HTTPResponseStatus) {
+            self.result = .init(data: data, errors: errors)
+            self.status = status
+        }
     }
 
     /// HTTP-based GraphQL request
@@ -30,5 +42,23 @@ extension Pioneer {
 
         /// HTTP method for this request
         public var method: HTTPMethod
+
+        public init(request: GraphQLRequest, headers: HTTPHeaders, method: HTTPMethod) {
+            self.request = request
+            self.headers = headers
+            self.method = method
+        }
+
+        public init(
+            query: String,
+            operationName: String? = nil,
+            variables: [String: Map]? = nil,
+            headers: HTTPHeaders, 
+            method: HTTPMethod    
+        ) {
+            self.request = .init(query: query, operationName: operationName, variables: variables)
+            self.headers = headers
+            self.method = method
+        }
     }
 }
