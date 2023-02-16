8e86200c5743e3f34a9f078dde5bf2b0801da335
Vincent <thisoneis4business@gmail.com>
2023-02-16 20:22:49 +1300

    feat: Use WebSocketClient.ID

--- Sources/Pioneer/WebSocket/Probe/Probe.swift
+++ Sources/Pioneer/WebSocket/Probe/Probe.swift
@@ -18,25 +18,19 @@ extension Pioneer {
         private let proto: SubProtocol.Type
 
         init(
-            schema: GraphQLSchema, resolver: Resolver, proto: SubProtocol.Type
+            schema: GraphQLSchema,
+            resolver: Resolver,
+            proto: SubProtocol.Type
         ) {
             self.schema = schema
             self.resolver = resolver
             self.proto = proto
         }
 
-        init(
-            schema: Schema<Resolver, Context>, resolver: Resolver, proto: SubProtocol.Type
-        ) {
-            self.schema = schema.schema
-            self.resolver = resolver
-            self.proto = proto
-        }
-
         // MARK: - Private mutable states
 
-        private var clients: [UUID: WebSocketClient] = [:]
-        private var drones: [UUID: Drone] = [:]
+        private var clients: [WebSocketClient.ID: WebSocketClient] = [:]
+        private var drones: [WebSocketClient.ID: Drone] = [:]
 
         // MARK: - Event callbacks
 
@@ -46,14 +40,14 @@ extension Pioneer {
         }
 
         /// Deallocate the space from a closing process
-        func disconnect(for cid: UUID) async {
+        func disconnect(for cid: WebSocketClient.ID) async {
             await drones[cid]?.acid()
             clients.delete(cid)
             drones.delete(cid)
         }
 
         /// Long running operation require its own actor, thus initialing one if there were none prior
-        func start(for cid: UUID, with oid: String, given gql: GraphQLRequest) async {
+        func start(for cid: WebSocketClient.ID, with oid: String, given gql: GraphQLRequest) async {
             guard let client = clients[cid] else {
                 return
             }
@@ -69,7 +63,7 @@ extension Pioneer {
         }
 
         /// Short lived operation is processed immediately and pipe back later
-        func once(for cid: UUID, with oid: String, given gql: GraphQLRequest) async {
+        func once(for cid: WebSocketClient.ID, with oid: String, given gql: GraphQLRequest) async {
             guard let client = clients[cid] else {
                 return
             }
@@ -96,7 +90,7 @@ extension Pioneer {
         }
 
         /// Stopping any operation to client specific actor
-        func stop(for cid: UUID, with oid: String) async {
+        func stop(for cid: WebSocketClient.ID, with oid: String) async {
             await drones[cid]?.stop(for: oid)
         }
 
@@ -106,27 +100,20 @@ extension Pioneer {
             client.out(GraphQLMessage(id: oid, type: proto.complete).jsonString)
         }
 
-        // MARK: - Utility methods
-
         /// Build context and execute short-lived GraphQL Operation inside an event loop
         private func execute(_ gql: GraphQLRequest, client: WebSocketClient) -> Task<GraphQLResult, Error> {
             Task { [unowned self] in
                 let ctx = try await client.context(gql)
-                return try await self.executeOperation(for: gql, with: ctx, using: client.ev)
+                return try await executeGraphQL(
+                    schema: self.schema,
+                    request: gql.query,
+                    resolver: self.resolver,
+                    context: ctx,
+                    eventLoopGroup: client.ev,
+                    variables: gql.variables,
+                    operationName: gql.operationName
+                )
             }
-        }
-
-        /// Execute short-lived GraphQL Operation
-        private func executeOperation(for gql: GraphQLRequest, with ctx: Context, using eventLoop: EventLoopGroup) async throws -> GraphQLResult {
-            try await executeGraphQL(
-                schema: self.schema,
-                request: gql.query,
-                resolver: self.resolver,
-                context: ctx,
-                eventLoopGroup: eventLoop,
-                variables: gql.variables,
-                operationName: gql.operationName
-            )
         }
     }
 }
